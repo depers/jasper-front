113603f843a1e463ad635e94cc4e8bb40f5b064c
depers <dev_fengxiao@163.com>
2024-07-27 22:18:18 +0800

    首页和文章页支持返回顶部功能

--- js/common.js
+++ js/common.js
@@ -5,3 +5,19 @@ function loadYear() {
     var date = new Date();
     $("#updateYear").text(date.getFullYear());
 }
+
+function handleScroll() {
+    // window.scrollY：文档在垂直方向已滚动的像素值，clientHeight代表浏览器窗口的高度
+    if (window.scrollY > rootElement.clientHeight) {
+        scrollToTopBtn.classList.add("showBtn")
+    } else {
+        scrollToTopBtn.classList.remove("showBtn")
+    }
+}
+
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
